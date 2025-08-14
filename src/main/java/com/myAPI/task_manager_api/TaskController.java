e81dfa5875c957e466b24ce5d4381e9f7bdd88db
Sergio Torres <sergio.arango166@gmail.com>
2025-08-14 18:01:58 -0500

    Nueva funcionalidad update task

--- src/main/java/com/myAPI/task_manager_api/TaskController.java
+++ src/main/java/com/myAPI/task_manager_api/TaskController.java
@@ -2,7 +2,11 @@ package com.myAPI.task_manager_api;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -19,5 +23,21 @@ public class TaskController {
     @PostMapping
     public Task createTask(@RequestBody Task task) {
         return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        Optional<Task> existingTask = taskRepository.findById(id);
+
+        if (existingTask.isPresent()) {
+            Task taskToUpdate = existingTask.get();
+            taskToUpdate.setTitle(updatedTask.getTitle());
+            taskToUpdate.setDescription(updatedTask.getDescription());
+            taskToUpdate.setCompleted(updatedTask.isCompleted());
+            Task savedTask = taskRepository.save(taskToUpdate);
+            return new ResponseEntity<>(savedTask, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
